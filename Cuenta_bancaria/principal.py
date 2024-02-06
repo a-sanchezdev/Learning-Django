ce3d04d72137f1e222a0731c49b2e8878dcbe5d7
a-sanchezdev <asanchez@aggregatum.com>
2024-02-06 15:07:15 -0600

    Cuenta bancaria

---
+++ Cuenta_bancaria/principal.py
@@ -0,0 +1,32 @@
+import banco
+
+def preguntar():
+    print("Bienvenido a Farmacia Python")
+
+    while True:
+        print("[P]-Perfumeria\n[F]-Farmacia\n[C]-Comestica")
+        try:
+            mi_rubro = input("Elija su rubro: ").upper()
+            ["P","F","C"].index(mi_rubro)
+        except ValueError:
+            print("Esa no es una opcion valida")
+        else:
+            break
+
+
+    banco.decorador(mi_rubro)
+
+def inicio():
+    while True:
+        preguntar()
+        try:
+            otro_turno = input("Quieres sacar otro turno? [S] [N]: ").upper()
+            ["S","N"].index(otro_turno)
+        except ValueError:
+            print("Esa no es una opcion valida")
+        else:
+            if otro_turno == "N":
+                print("Gracias por su visita")
+                break
+
+inicio()
